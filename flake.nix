3e418625e25d72a0e475a78fbf56717b7f20d488
Kayla Firestack <kfirestack@mbta.com>
2023-09-07 17:51:11 -0400

    fix: use rust-overlay to get latest toolchain

--- flake.nix
+++ flake.nix
@@ -14,17 +14,25 @@
 			url = "github:numtide/flake-utils";
 		};
 
+		rust-overlay = {
+			url = "github:oxalica/rust-overlay";
+			inputs = {
+				nixpkgs.follows = "nixpkgs";
+				flake-utils.follows = "flake-utils";
+			};
+		};
 	};
 
-	outputs = { self, nixpkgs, crane, flake-utils, ... }:
+	outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = import nixpkgs {
 					inherit system;
+					overlays = [ (import rust-overlay) ];
 				};
 
-				craneLib = crane.lib.${system};
-				src = ./.;
+				craneLib = (crane.mkLib pkgs).overrideToolchain pkgs.rust-bin.stable.latest.default;
+				src = self;
 
 				# Build *just* the cargo dependencies, so we can reuse
 				# all of that work (e.g. via cachix) when running in CI
